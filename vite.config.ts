9d402b29caaa10c4e6ec1d1fbccee420811226eb
ilyas <ilyasswadjinny@gmail.com>
2024-04-01 17:38:27 +0200

    fixing github page

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+
+const ASSET_URL = process.env.ASSET_URL || '';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   //Base /le-ou-la-vite
-  base: '/le-ou-la-vite/'
+  base: ASSET_URL,
 })
