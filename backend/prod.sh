08571fb8b284fb2bec9429d8b3e24e824e01f511
ilyas <ilyas.wadjinny@gmail.com>
2025-11-15 04:14:07 +0100

    fix

--- backend/prod.sh
+++ backend/prod.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR=$(dirname $(realpath $0))
 ENV_FILE=$SCRIPT_DIR/../.env.production
 echo "ENV_FILE: $ENV_FILE"
 . $ENV_FILE
-echo "API_PORT: $API_PORT"
+echo "API_PORT: $VITE_API_PORT"
 # single process
 $(poetry env activate)
-uvicorn backend.main:app --host 0.0.0.0 --port $API_PORT --workers 4
+uvicorn backend.main:app --host 0.0.0.0 --port $VITE_API_PORT --workers 4
