d13cef01adf4161c874a1f3ea05a5857d3361751
ilyas <ilyas.wadjinny@gmail.com>
2025-11-15 03:09:16 +0100

    fix

--- backend/prod.sh
+++ backend/prod.sh
@@ -2,6 +2,6 @@ SCRIPT_DIR=$(dirname $(realpath $0))
 ENV_FILE=$SCRIPT_DIR/../.env
 echo "ENV_FILE: $ENV_FILE"
 . $ENV_FILE
-echo "API_PORT: $API_PORT"
+echo "API_PORT: $VITE_API_PORT"
 # single process
-uvicorn backend.main:app --host 0.0.0.0 --port $API_PORT --workers 4
+uvicorn backend.main:app --host 0.0.0.0 --port $VITE_API_PORT --workers 4
