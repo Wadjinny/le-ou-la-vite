f52f1820966f2f4d62a3ca9b1ce6dbf782f2d73a
ilyas <ilyas.wadjinny@gmail.com>
2025-11-15 19:17:55 +0100

    Refactor SearchWord and WordDefinition components to improve user experience with loading indicators and navigation. Introduce a new Word component for better code organization and update state management for search input.

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+export const NavBar = (props: Props) => {
+  return (
+    <div className="bg-slate-200 text-center text-4xl py-4 border-b-2 border-slate-200 mb-3">
+        <h1 className="text-4xl text-slate-600 font-light font-serif">Le ou La</h1>
+    </div>  
+  )
+}
